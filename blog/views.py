50568c747070b47f1f674c19c06d876272197696
CindyKatoni <katmuema@gmail.com>
2020-10-28 01:23:23 +0300

    Created templates for the app

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Cindy Nduku',
+        'title': 'My First Post',
+        'content': 'First Post Content',
+        'date_posted': 'October,28,2020'
+    },
+     {
+        'author': 'Valerie Gaya',
+        'title': 'My Second Post',
+        'content': 'Second Post Content',
+        'date_posted': 'October,29,2020'
+    },
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')    
+    return render(request, 'blog/about.html',{'title':'About'})    
